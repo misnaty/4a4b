59e2eda6bff9e54afe9fcda9929a0814743cbde7
misnaty <narytaccess@gmail.com>
2026-05-26 14:28:05 -0300

    meows.zip

---
+++ meows/src/main/java/com/meows/meows/MeowsApplication.java
@@ -0,0 +1,13 @@
+package com.meows.meows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowsApplication.class, args);
+	}
+
+}
---
+++ meows/src/test/java/com/meows/meows/MeowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meows.meows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
